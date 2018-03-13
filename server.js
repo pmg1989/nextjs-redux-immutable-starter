e9c53d634d203ff1c7cea40fcd1767070be2ac99
felixpmg <felix.pan@newband.com>
2018-03-13 12:18:52 +0800

    fix env

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const path = require('path')
 const compression = require('compression')
 const next = require('next')
 
+const routes = require('./routes')
+
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
-const handler = app.getRequestHandler()
+const handler = routes.getRequestHandler(app)
 
 app.prepare()
 .then(() => {
